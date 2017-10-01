a513c76c292af1cb2403bd9a00039d8693da2f16
Stephan Seifermann <seiferma@users.noreply.github.com>
2017-10-01 05:19:08 +0200

    Adjusted current positions.

--- src/main/java/io/github/selchapp/api/config/UserDetailsServiceBasedAuthentificationConfig.java
+++ src/main/java/io/github/selchapp/api/config/UserDetailsServiceBasedAuthentificationConfig.java
@@ -1,20 +1,29 @@
 package io.github.selchapp.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class UserDetailsServiceBasedAuthentificationConfig extends GlobalAuthenticationConfigurerAdapter  {
-	
+public class UserDetailsServiceBasedAuthentificationConfig extends GlobalAuthenticationConfigurerAdapter {
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Override
-	  public void init(AuthenticationManagerBuilder auth) throws Exception {
-	    auth.userDetailsService(userDetailsService);
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder(11);
+		return encoder;
+	}
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO user (id, nickname, password) VALUES ('1', 'test', 'test');
-INSERT INTO user (id, nickname, password, lat, lng) VALUES ('2', 'Stephan', 'test', '47.4917816', '12.2715673');
-INSERT INTO user (id, nickname, password, lat, lng) VALUES ('3', 'Robert', 'test', '47.4943406', '12.2655373');
-INSERT INTO user (id, nickname, password, lat, lng) VALUES ('4', 'Valentin', 'test', '47.4943406', '12.2655373');
+INSERT INTO user (id, nickname, password) VALUES ('1', 'test', '$2a$11$b3gp3TbSIacbDQGKnFamS.gLm4WBihMdl5BAfAz7MWSHulqWPhcwe');
+INSERT INTO user (id, nickname, password, lat, lng) VALUES ('2', 'Stephan', '$2a$11$.u7sPdfsChA9QrKhKemLL.laQlERJvhjB/4/uU4sajBOgBSDRAWQC', '47.465210', '12.203946');
+INSERT INTO user (id, nickname, password, lat, lng) VALUES ('3', 'Robert', '$2a$11$fR7kh.L9PUh/5c82o5hg5udRKLdMZOa2I9V/ZKUArUEX1cfS4zLiC', '47.480173', '12.192431');
+INSERT INTO user (id, nickname, password, lat, lng) VALUES ('4', 'Valentin', '$2a$11$o6/ctPNl8HIo71PViJhBPeK.ixOMC3h3TUZjp1DCnoWwnLZ3dz8Bq', '47.456421', '12.185614');
 
 INSERT INTO team (id, name) VALUES ('1', 'Selches');
 
