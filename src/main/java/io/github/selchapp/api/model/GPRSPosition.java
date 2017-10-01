830e56c4d731aa1bbae852a8da063d284d23ae6c
Stephan Seifermann <seiferma@users.noreply.github.com>
2017-10-01 04:58:36 +0200

    Adjusted setters/getters

--- src/main/java/io/github/selchapp/api/model/GPRSPosition.java
+++ src/main/java/io/github/selchapp/api/model/GPRSPosition.java
@@ -7,32 +7,32 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Embeddable
 public class GPRSPosition {
-	
-	@Column(nullable=true)
+
+	@Column(nullable = true)
 	private double lat;
-	
-	@Column(nullable=true)
+
+	@Column(nullable = true)
 	private double lng;
 
-	public double getLatitude() {
+	public double getLat() {
 		return lat;
 	}
 
-	public void setLatitude(double latitude) {
-		this.lat = latitude;
+	public void setLat(double lat) {
+		this.lat = lat;
 	}
 
-	public double getLongitude() {
+	public double getLng() {
 		return lng;
 	}
 
-	public void setLongitude(double longitude) {
-		this.lng = longitude;
+	public void setLng(double lng) {
+		this.lng = lng;
 	}
-	
+
 	@JsonIgnore
 	public boolean isValid() {
 		return lat != 0 && lng != 0;
 	}
-	
+
 }
